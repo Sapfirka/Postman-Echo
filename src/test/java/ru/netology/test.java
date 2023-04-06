f06af298254437c38e774709692d40d5e8b22c09
Sapfirka <sapfirka@mail.ru>
2023-04-06 23:52:48 +0300

    First commit

---
+++ src/test/java/ru/netology/test.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class test {
+    @Test
+    void shouldPostBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("01.01.2023") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("01.01.2023"))
+        ;
+    }
+}
